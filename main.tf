e282b29196adb4a1084e2f0116bfef83f66727ce
Kishor Satishkumar Kashid <144726530+kishor-kashid@users.noreply.github.com>
2024-10-15 17:47:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "main_vpc" {
 # Create public subnets dynamically based on VPC CIDR and availability zones
 resource "aws_subnet" "public_subnets" {
   count                   = var.subnet_count
-  vpc_id                  = aws_vpc.main_vpc.id
+  vpc_id                  = aws_vpc.main_vpc.bad
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index) # Generating subnet CIDRs dynamically
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
